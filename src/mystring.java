4054eaafcd4d399181f8191272d77e384d8f6b06
taghreed-1 <s2221160602@ku.edu.kw>
2026-09-21 22:41:31 +0300

    fixed

--- src/mystring.java
+++ src/mystring.java
@@ -14,7 +14,8 @@ Text= "";
 for ( i=0; i<n; i++) {
 if (Temp[i] >= 'A' && Temp[i] <= 'Z')
 Temp[i]= (char)(Temp[i] - 'A' + 'a') ;
-if (Temp[i] >= 'a' && Temp[i] <= 'z')
+else if (Temp[i] >= 'a' && Temp[i] <= 'z')
+
 Temp[i]= (char)(Temp[i] - 'a' + 'A') ;
 Text += Temp[i];
 }
