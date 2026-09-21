4085015f3828a5b55c2cc2946532c4eed7a462d2
taghreed-1 <s2221160602@ku.edu.kw>
2026-09-21 22:39:06 +0300

    initial commit

---
+++ src/mystring.java
@@ -0,0 +1,23 @@
+public class mystring {
+private String Text;
+public mystring() {
+Text = null;
+}
+public void setString(String t){
+Text = t;
+}
+public String invert( ){
+int i , n = Text.length();
+char [] Temp;
+Temp = Text.toCharArray();
+Text= "";
+for ( i=0; i<n; i++) {
+if (Temp[i] >= 'A' && Temp[i] <= 'Z')
+Temp[i]= (char)(Temp[i] - 'A' + 'a') ;
+if (Temp[i] >= 'a' && Temp[i] <= 'z')
+Temp[i]= (char)(Temp[i] - 'a' + 'A') ;
+Text += Temp[i];
+}
+return Text;
+}
+}
